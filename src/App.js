a9384df3295dced4dc5aa37e02e670b5798d33e7
Александр Бурлаков <aburlakov@cleverence.ru>
2022-03-05 10:32:46 +0300

    Components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,27 @@ import './App.css';
 
 let App = () => (
         <div className="App">
-            <ul>
-                <li>css</li>
-                <li>html</li>
-                <li>js</li>
-                <li>react</li>
-            </ul>
+            <Header />
+            <Tech />
+        </div>
+);
+
+let Tech = () => (
+    <div>
+        <ul>
+            <li>css</li>
+            <li>html</li>
+            <li>js</li>
+            <li>react</li>
+        </ul>
+    </div>
+);
+
+let Header = () => (
+        <div>
+            <a>Home</a>
+            <a>News</a>
+            <a>Messages</a>
         </div>
 );
 
